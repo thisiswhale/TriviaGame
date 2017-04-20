70395df0522715944ab3219d40bb7bba407a9373
William Leung <William Leung>
2017-04-20 15:57:23 -0700

    reset, start button works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,11 @@
-var wrongAnswers = 0;
-var rightAnswers = 0;
-var missed = 0;
-var index = 9;
+var wrongAnswers;
+var rightAnswers;
+var missed;
+var index;
 
 
 //number counter 
-var timeStart = 5;
+var timeStart;
 $(".timer").html(timeStart)
 
 //interval ID when executing the "run" function
@@ -22,7 +22,7 @@ function stop(){
 function countdown() {
     timeStart--;
     $(".timer").html(timeStart);
-
+    console.log(timeStart);
     if (timeStart === 0) {
     	stop();
     	$(".right-wrong").html("Times Up!! <strong>Correct answer is " +trivia[index].answer +".</strong>");
@@ -33,31 +33,56 @@ function countdown() {
     }
 }
 
+var width = 200;
+var image0 = new Image(width);
+var image1 = new Image(width);
+var image2 = new Image(width);
+var image3 = new Image(width); 
+var image4 = new Image(width);
+var image5 = new Image(width);
+var image6 = new Image(width);
+var image7 = new Image(width);
+var image8 = new Image(width);
+var image9 = new Image(width);
+var image10 = new Image(width);
+
+image0.src = "assets/images/homer.gif";
+image1.src = "assets/images/africa.gif";
+image2.src ="assets/images/coke.gif";
+image3.src = "assets/images/interesting.gif";
+image4.src = "assets/images/coffeeface.gif";
+image5.src = "assets/images/pony-gargle.gif";
+image6.src = "assets/images/blackcoffee.gif";
+image7.src = "assets/images/blackcoffee.gif";
+image8.src = "assets/images/toomuch.gif";
+image9.src = "assets/images/espresso.gif";
+
+
 var trivia = [{
     question: "About how many cups of coffee does the average person drink daily?",
     choice: ["1", "3", "5", "8"],
     answer: "3",
-    image: "image 0"
+    image: image0.src
 }, {
     question: "Where did coffee first come from?",
     choice: ["Hawaii", "Madagascar", "South and Centra America", "Africa"],
     answer: "Africa",
-    image: "image 1"
+    image: image1.src
 }, {
     question: "A 'grande' coffee has as much caffeine as how many cans of cola?",
     choice: ["1", "3", "5", "7"],
     answer: "7",
-    image: "image 2"
+    image: image2.src
 }, {
     question: "Every year the average American drinks enough coffee to fill",
     choice: ["A swimming pool", "A bathtub", "a wading pool", "the ocean"],
     answer: "A bathtub",
-    image: "image 3"
+    image: image3.src
 }, {
     question: "What's the best place to store your coffee beans or ground coffee?",
     choice: ["Outside", "The counter", "The freezer", "The refrigerator"],
     answer: "The counter",
-    image: "image 3"
+    image: image4.src
 }, {
     question: "What's the best way to protect your teeth from the acid in coffee?",
     choice: ["Rinse your mouth with water", 
@@ -65,22 +90,22 @@ var trivia = [{
     		"Use a straw", 
     		"Drink something to balance the acid"],
     answer: "Rinse your mouth with water",
-    image: "image 3"
+    image: image5.src
 }, {
     question: "What percentage of coffee drinkers prefer their coffee black?",
     choice: ["30%", "40%", "50%", "60%"],
     answer: "40%",
-    image: "image 3"
+    image: image6.src
 }, {
     question: "About how many milligrams of caffeine is the human body capable of absorbing?",
     choice: ["100", "200", "300", "400"],
     answer: "300",
-    image: "image 3"
+    image: image7.src
 }, {
     question: "How many milligrams of coffee are in a typical cup of coffee?",
     choice: ["50", "80", "120", "150"],
     answer: "150",
-    image: "image 3"
+    image: image8.src
 }, {
     question: "Where does the word espresso come from?",
     choice: ["From the Greek Expreco, meaning 'strong beverage'",
@@ -89,10 +114,11 @@ var trivia = [{
         "From a drink served in early Viennese coffeehouses"
     ],
     answer: "From its Latin root meaning 'under pressure'",
-    image: "image 3"
+    image: image9.src
 }];
 
 function newTrivia() {
+	$(".image").empty();
     $(".trivia-question").html(trivia[index].question);
     $(".A").html(trivia[index].choice[0]);
     $(".B").html(trivia[index].choice[1]);
@@ -123,7 +149,8 @@ function nextTrivia() {
 function showImage() {
     //stops timer
     stop();
-    $(".image").html(trivia[index].image);
+    //$(".image").attr("src",trivia[index].image);
+    $(".image").html("<img src="+trivia[index].image +" width='200px'/>");
     //execute function in 5 seconds
     setTimeout(nextTrivia, 1000 * 3);
 }
@@ -131,23 +158,37 @@ function showImage() {
 function endGame() {
 	console.log("end game here");
     $(".result").html("Answers Correct: " + rightAnswers + "&nbsp; &nbsp; Answers Wrong: " + wrongAnswers + "&nbsp; &nbsp; Unanswered: " + missed);
-    //create a button to reset game
-    //$(".result").append("<button class=")
+	$("#start").html("Restart");
+    $("#start").show();
+    $(".box").hide();
+
+
 }
 
-function reset() {
+function newGame() {
+	timeStart = 5;
+	$(".timer").html(timeStart);
     wrongAnswers = 0
     rightAnswers = 0
     missed = 0;
-    index = 0;
+    $(".result").html("");
+    $(".box").show();
+    
+    index = 9;
     newTrivia();
     run();
 }
 
+$("#start").on('click', function(){
+	$(".container").show();
+ 	$(".background").hide();
+ 	$("#start").hide();
+ 	newGame();
+ });
+
 window.onload = function() {
-    
-	run();
-	newTrivia();
+    $(".background").html("<img src='assets/images/game-coffee.gif'>");
+	$(".container").hide();
 
     $(".answer-choice").on("click", function() {
         console.log(index);
@@ -166,6 +207,6 @@ window.onload = function() {
 
 };
 
-//add gifs to coresponding questions
+
 //makes things pretty
-//add reset button
+//add newGame button
